5ca0cf4d1a34e3971b70c7272585b7c194c06c1f
Albert <acastemoreno@gmail.com>
2020-05-28 15:58:20 -0500

    device routes and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_192917) do
+ActiveRecord::Schema.define(version: 2020_05_28_205326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,11 +71,16 @@ ActiveRecord::Schema.define(version: 2020_05_28_192917) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "email", null: false
+    t.string "email", default: "", null: false
     t.integer "memes_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
