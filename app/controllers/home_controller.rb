005d6b0c883b2bfd76706471a48ed652c097f504
Albert <acastemoreno@gmail.com>
2020-05-29 11:20:11 -0500

    change name of user seed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @memes = Meme.order('created_at DESC').group_by { |m| m.created_at.to_date }
+    @memes = Meme.preload(:owner).order('created_at DESC').group_by { |m| m.created_at.to_date }
   end
 end
