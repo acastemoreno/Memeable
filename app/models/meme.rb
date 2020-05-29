b97c700bf1ee063f4523d7d55ce4c34bdae6a79f
Albert <acastemoreno@gmail.com>
2020-05-29 12:51:50 -0500

    memes_count for users

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -3,7 +3,7 @@ class Meme < ApplicationRecord
   
   ### Associations
   belongs_to :category
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', counter_cache: :memes_count
   has_and_belongs_to_many :tags
   has_many :comments
   has_many :commentators, through: :comments, source: :user
