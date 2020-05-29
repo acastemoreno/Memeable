8d80b02b4caaba3a9b43916136e10b853064dd4b
Albert <acastemoreno@gmail.com>
2020-05-29 11:56:09 -0500

    category page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @memes = Meme.preload(:owner).order('created_at ASC').group_by { |m| m.created_at.to_date }
+    @dates = Meme.preload(:owner).order('created_at ASC').group_by { |m| m.created_at.to_date }
+  end
+
+  def categories
+    @categories = Category.preload(:memes)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,13 @@ user = User.create(username: "Albert", email: "acastemoreno@gma.com", password: 
 p user.errors
 user_votator = User.create(username: "Marvin", email: "vot@gmail.com", password: "123456", encrypted_password: BCrypt::Password.create("123456"))
 
-category = Category.find_or_create_by(name: "holi")
+category = Category.find_or_create_by(name: "fun")
+category2 = Category.find_or_create_by(name: "codeable")
 
 meme = Meme.find_or_create_by(title: "today1", url_source: "https://www.pittsburghmagazine.com/content/uploads/2020/03/IMG_2013-771x1024.jpg", owner: user, category: category, type: "holi")
 
 Meme.find_or_create_by(title: "amiguito2", url_source: "https://imgs.xkcd.com/comics/mbmbam.png", owner: user, category: category, type: "holi", created_at: "2020-04-20")
-Meme.find_or_create_by(title: "today2", url_source: "https://e.rpp-noticias.io/normal/2019/08/29/175117_834052.png", owner: user_votator, category: category, type: "holi")
+Meme.find_or_create_by(title: "today2", url_source: "https://e.rpp-noticias.io/normal/2019/08/29/175117_834052.png", owner: user_votator, category: category2, type: "holi")
 
 
 meme.tags << tag
