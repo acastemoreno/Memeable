f43122d1cff3fd85e64111a5dbdbda0bd68e4cde
Albert <acastemoreno@gmail.com>
2020-05-29 19:49:53 -0500

    create meme

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,5 +1,25 @@
 class MemesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def show
     @meme = Meme.find(params[:id])
+  end
+
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = Meme.new(meme_params.merge(owner: current_user))
+    if @meme.save
+      redirect_to meme_path(@meme)
+    else
+      render :new
+    end
+  end
+
+  private
+  def meme_params
+    params.require(:meme).permit(:title, :url_source, :type, :category_id, :owner)
   end
 end
